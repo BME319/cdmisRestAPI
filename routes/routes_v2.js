4742b27265949e3e1d8ef33ed45626b39e9bb5d5
SageChi <sqchi1991@163.com>
2017-07-04 14:22:45 +0800

    add routes_v2.js

--- routes/routes_v2.js
+++ routes/routes_v2.js
@@ -19,7 +19,8 @@ var getNoMid = require('../middlewares/getNoMid'),
     aclChecking = require('../middlewares/aclChecking');
 
 // controllers
-var aclsettingCtrl = require('../controllers/aclsetting_controller');
+var aclsettingCtrl = require('../controllers/aclsetting_controller'),
+    alluserCtrl = require('../controllers/alluser_controller');
 
 
 module.exports = function(app,webEntry, acl) {
@@ -29,6 +30,7 @@ module.exports = function(app,webEntry, acl) {
   //   res.send("Server Root");
   // });
 
+  // csq 
   app.post(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.addUserRoles(acl));
   app.post(version + '/acl/removeUserRoles', tokenManager.verifyToken(), aclsettingCtrl.removeUserRoles(acl));
   app.get(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.userRoles(acl));
@@ -48,6 +50,29 @@ module.exports = function(app,webEntry, acl) {
   app.get(version + '/acl/areAnyRolesAllowed', tokenManager.verifyToken(), aclsettingCtrl.areAnyRolesAllowed(acl));
   app.get(version + '/acl/resources', tokenManager.verifyToken(), aclsettingCtrl.whatResources(acl));
 
+  // wf
+  app.get(version + '/alluser/userList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(0));
+  app.get(version + '/alluser/doctorList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(1));
+  app.get(version + '/alluser/patientList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(2));
+  app.get(version + '/alluser/nurseList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(3));
+  app.get(version + '/alluser/insuranceList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(4));
+  app.get(version + '/alluser/healthList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(5));
+  app.get(version + '/alluser/adminList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(6));
+  app.post(version + '/alluser/alluser',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.updateAlluserList);
+  
+  app.post(version + '/alluser/register',  tokenManager.verifyToken(), alluserCtrl.registerTest(acl),getNoMid.getNo(1), alluserCtrl.register(acl));
+  app.post(version + '/alluser/cancelUser',  tokenManager.verifyToken(), alluserCtrl.checkAlluser,alluserCtrl.cancelAlluser);
+  app.post(version + '/alluser/unionid',  tokenManager.verifyToken(), alluserCtrl.setOpenId, alluserCtrl.checkBinding, alluserCtrl.setOpenIdRes);
+  app.post(version + '/alluser/openId',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.setMessageOpenId);
+  app.get(version + '/alluser/openId',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.getMessageOpenId);
+  app.post(version + '/alluser/reset',  tokenManager.verifyToken(), alluserCtrl.reset);
+  app.post(version + '/alluser/login',  tokenManager.verifyToken(), alluserCtrl.openIdLoginTest,alluserCtrl.checkBinding,alluserCtrl.login);
+  app.post(version + '/alluser/logout',   tokenManager.verifyToken(), alluserCtrl.logout);
+  app.get(version + '/alluser/userID',   tokenManager.verifyToken(), alluserCtrl.getAlluserID);
+  app.post(version + '/alluser/sms',   tokenManager.verifyToken(), alluserCtrl.sendSMS);
+  app.get(version + '/alluser/sms',   tokenManager.verifyToken(), alluserCtrl.verifySMS);
+  app.get(version + '/alluser/agreement',   tokenManager.verifyToken(), alluserCtrl.getAlluserAgreement);
+  app.post(version + '/alluser/agreement',   tokenManager.verifyToken(), alluserCtrl.updateAlluserAgreement);
 
 };
 
