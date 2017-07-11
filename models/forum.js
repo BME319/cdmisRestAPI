c2bf3156f4ddff3b05a206b6eac42dfb21060819
xiaopangjiang <zjujiangyifan@163.com>
2017-07-11 19:40:52 +0800

    models和middlewares标准化修改

--- models/forum.js
+++ models/forum.js
@@ -1,95 +1,91 @@
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+var Reply = require('../models/reply')
 
 var forumSchema = new mongoose.Schema({
-	postId:String,
-	type:Number,
-	board:Number,
-	status:Number,
-	sponsorId:String,
-	sponsorName:String,
-	title:String,
-	subject:String,
-	time:Date,
-	content:[],
-	hits:Number,
-	praises:Number,
-	replyCount:Number,
-	replies:[Reply],
-	revisionInfo:{	
-		operationTime:Date,
-		userId:String,
-		userName:String,
-		terminalIP:String
-	}
-});
+  postId: String,
+  type: Number,
+  board: Number,
+  status: Number,
+  sponsorId: String,
+  sponsorName: String,
+  title: String,
+  subject: String,
+  time: Date,
+  content: [],
+  hits: Number,
+  praises: Number,
+  replyCount: Number,
+  replies: [Reply],
+  revisionInfo: {
+    operationTime: Date,
+    userId: String,
+    userName: String,
+    terminalIP: String
+  }
+})
 
+var forumModel = mongoose.model('forum', forumSchema)
 
-forumModel = mongoose.model('forum', forumSchema);
-
-function Forum(forum) {
-	this.forum = forum;
+function Forum (forum) {
+  this.forum = forum
 }
 
-forum.prototype.save = function(callback) {
-	var forum = this.forum;
-	var newforum = new forumModel(forum);
-	newForum.save(function(err, forumItem) {
-		if (err) {
-			return callback(err);
-		}
-		callback(null, forumItem);
-	});
+Forum.prototype.save = function (callback) {
+  var forum = this.forum
+  var newForum = new forumModel(forum)
+  newForum.save(function (err, forumItem) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, forumItem)
+  })
 }
 
-forum.getOne = function(query, callback, opts, fields, populate) {
-	var options = opts || {};
-	var fields = fields || null;
-	var populate = populate || '';
+Forum.getOne = function (query, callback, opts, fields, populate) {
+  var options = opts || {}
+  var _fields = fields || null
+  var _populate = populate || ''
 
-	forumModel
-		.findOne(query, fields, opts)
-		.populate(populate)
-		.exec(function(err, forumInfo) {
-			if(err){
-				return callback(err);
-			}
-			callback(null, forumInfo);
-		});
-};
+  forumModel
+  .findOne(query, _fields, options)
+  .populate(_populate)
+  .exec(function (err, forumInfo) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, forumInfo)
+  })
+}
 
+Forum.getSome = function (query, callback, opts, fields, populate) {
+  var options = opts || {}
+  var _fields = fields || null
+  var _populate = populate || ''
+  forumModel
+  .find(query, _fields, options)
+  .populate(_populate)
+  .exec(function (err, forums) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, forums)
+  })
+}
 
-forum.getSome = function(query, callback, opts, fields, populate) {
-	var options = opts || {};
-	var fields = fields || null;
-	var populate = populate || '';
-	forumModel
-		.find(query, fields, options)
-		.populate(populate)
-		.exec(function(err, forums) {
-			if(err) {
-				return callback(err);
-			}
-			callback(null, forums);
-		});
-};
+Forum.updateOne = function (query, obj, callback, opts, populate) {
+  var options = opts || {}
+  var _populate = populate || ''
 
-forum.updateOne = function(query, obj, callback, opts, populate) {
-	var options = opts || {};
-	var populate = populate || '';
+  forumModel
+  .findOneAndUpdate(query, obj, options)
+  .populate(_populate)
+  .exec(function (err, upforum) {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, upforum)
+  })
+}
 
-	forumModel
-		.findOneAndUpdate(query, obj, options)
-		.populate(populate)
-		.exec(function(err, upforum) {
-			if(err){
-				return callback(err);
-			}
-			callback(null, upforum);
-		});
-};
-
-
-
-
-module.exports = Forum;
+module.exports = Forum
--- models/labtestImport.js
+++ models/labtestImport.js
@@ -1,137 +1,131 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 var labtestImportSchema = new mongoose.Schema({
-  userId: String,	
-  labtestId: String, 
-  time: Date, 
+  userId: String,
+  labtestId: String,
+  time: Date,
   type: {
-    type: String, 
-    //SCr血肌酐, 单位umol/L, mg/dL
-    //GFR肾小球滤过率, ml/min
-    //ALB血白蛋白, g/L
-    //PRO尿蛋白, mg/d
+    type: String,
+    // SCr血肌酐, 单位umol/L, mg/dL
+    // GFR肾小球滤过率, ml/min
+    // ALB血白蛋白, g/L
+    // PRO尿蛋白, mg/d
     enum: ['SCr', 'GFR', 'ALB', 'PRO']
-  }, 
-  value: Number, 
-  unit: String, 
+  },
+  value: Number,
+  unit: String,
   importer: {
-    type: mongoose.Schema.Types.ObjectId, 
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'alluser'
-  }, 
-  importTime: Date, 
+  },
+  importTime: Date,
   photoId: String
-  // SCr: Number, //血肌酐 
-  // SCrunit: String, 
+  // SCr: Number, //血肌酐
+  // SCrunit: String,
   // GFR: Number, //肾小球滤过率
-  // GFRunit: String, 
+  // GFRunit: String,
   // ALB: Number, //血白蛋白
-  // ALBunit: String, 
+  // ALBunit: String,
   // PRO: Number, //尿蛋白
   // PROunit: String
-});
+})
 
+var labtestImportModel = mongoose.model('labtestImport', labtestImportSchema)
 
-var labtestImportModel = mongoose.model('labtestImport', labtestImportSchema);
-
-function LabtestImport(labtestImport) {
-  this.labtestImport = labtestImport;
+function LabtestImport (labtestImport) {
+  this.labtestImport = labtestImport
 }
 
-LabtestImport.prototype.save = function(callback) {
-  var labtestImport = this.labtestImport;
-  var newLabtestImport = new labtestImportModel(labtestImport);
-  newLabtestImport.save(function(err, labtestImportItem) {
+LabtestImport.prototype.save = function (callback) {
+  var labtestImport = this.labtestImport
+  var newLabtestImport = new labtestImportModel(labtestImport)
+  newLabtestImport.save(function (err, labtestImportItem) {
     if (err) {
-      return callback(err);
+      return callback(err)
     }
-    callback(null, labtestImportItem);
-  });
+    callback(null, labtestImportItem)
+  })
 }
 
-LabtestImport.getOne = function(query, callback, opts, fields, populate) {
-  var options = opts || {};
-  var fields = fields || null;
-  var populate = populate || '';
+LabtestImport.getOne = function (query, callback, opts, fields, populate) {
+  var options = opts || {}
+  var _fields = fields || null
+  var _populate = populate || ''
 
   labtestImportModel
-    .findOne(query, fields, opts)
-    .populate(populate)
-    .exec(function(err, labtestImportInfo) {
-      if(err){
-        return callback(err);
+    .findOne(query, _fields, options)
+    .populate(_populate)
+    .exec(function (err, labtestImportInfo) {
+      if (err) {
+        return callback(err)
       }
-      callback(null, labtestImportInfo);
-    });
-};
+      callback(null, labtestImportInfo)
+    })
+}
 
-
-LabtestImport.getSome = function(query, callback, opts, fields, populate) {
-  var options = opts || {};
-  var fields = fields || null;
-  var populate = populate || '';
+LabtestImport.getSome = function (query, callback, opts, fields, populate) {
+  var options = opts || {}
+  var _fields = fields || null
+  var _populate = populate || ''
   labtestImportModel
-    .find(query, fields, options)
-    .populate(populate)
-    .exec(function(err, labtestImports) {
-      if(err) {
-        return callback(err);
+    .find(query, _fields, options)
+    .populate(_populate)
+    .exec(function (err, labtestImports) {
+      if (err) {
+        return callback(err)
       }
-      callback(null, labtestImports);
-    });
-};
+      callback(null, labtestImports)
+    })
+}
 
-LabtestImport.updateOne = function(query, obj, callback, opts, populate) {
-  var options = opts || {};
-  var populate = populate || '';
+LabtestImport.updateOne = function (query, obj, callback, opts, populate) {
+  var options = opts || {}
+  var _populate = populate || ''
 
   labtestImportModel
     .findOneAndUpdate(query, obj, options)
-    .populate(populate)
-    .exec(function(err, uplabtestImport) {
-      if(err){
-        return callback(err);
+    .populate(_populate)
+    .exec(function (err, uplabtestImport) {
+      if (err) {
+        return callback(err)
       }
-      callback(null, uplabtestImport);
-    });
-};
+      callback(null, uplabtestImport)
+    })
+}
 
 LabtestImport.update = function (query, obj, callback, opts, populate) {
-  var options = opts || {};
-  var populate = populate || '';
+  var options = opts || {}
+  var _populate = populate || ''
 
   labtestImportModel
-  	.update(query, obj, options)
-  	.populate(populate) 
-  	.exec(function (err, uplabtestImport) {
-    	if (err) {
-      		return callback(err);
-    	}
-    callback(null, uplabtestImport);
-  });
-};
+    .update(query, obj, options)
+    .populate(_populate)
+    .exec(function (err, uplabtestImport) {
+      if (err) {
+        return callback(err)
+      }
+      callback(null, uplabtestImport)
+    })
+}
 
-LabtestImport.remove = function(query, callback) {
+LabtestImport.remove = function (query, callback) {
   labtestImportModel
     .remove(query)
-    .exec(function(err) {
-      callback(err);
-    });
+    .exec(function (err) {
+      callback(err)
+    })
+}
 
-};
-
-LabtestImport.removeOne = function(query, callback, opts) {
-  var options = opts || {};
+LabtestImport.removeOne = function (query, callback, opts) {
+  var options = opts || {}
 
   labtestImportModel
-    .findOneAndRemove(query, options, function(err, health) {
+    .findOneAndRemove(query, options, function (err, health) {
       if (err) {
-        return callback(err);
+        return callback(err)
       }
-      callback(null, health);
-    });
-};
+      callback(null, health)
+    })
+}
 
-
-module.exports = LabtestImport;
-
-
+module.exports = LabtestImport
