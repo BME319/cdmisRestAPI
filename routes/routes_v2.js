ce29a363e5ab19edfda56bf2f6787ffcda7f38ec
xiaopangjiang <zjujiangyifan@163.com>
2017-07-11 11:09:19 +0800

    routes标准化修改

--- routes/routes_v2.js
+++ routes/routes_v2.js
@@ -5,32 +5,23 @@ var version = '/api/v2'
 // 3rd packages
 
 // self-defined configurations
-var config = require('../config')
+// var config = require('../config')
 
 // models
-var Wechat = require('../models/wechat')
+// var Wechat = require('../models/wechat')
 
 // middlewares
-var getNoMid = require('../middlewares/getNoMid'),
-  tokenManager = require('../middlewares/tokenManager'),
-  aclChecking = require('../middlewares/aclChecking')
+var getNoMid = require('../middlewares/getNoMid')
+var tokenManager = require('../middlewares/tokenManager')
+// var aclChecking = require('../middlewares/aclChecking')
 
 // controllers
-var aclsettingCtrl = require('../controllers_v2/aclsetting_controller'),
-  niaodaifuCtrl = require('../controllers_v2/niaodaifu_controller'),
-  alluserCtrl = require('../controllers_v2/alluser_controller')
+var aclsettingCtrl = require('../controllers_v2/aclsetting_controller')
+var niaodaifuCtrl = require('../controllers_v2/niaodaifu_controller')
+var alluserCtrl = require('../controllers_v2/alluser_controller')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-var reviewCtrl = require('../controllers_v2/review_controller')
-=======
-var reviewCtrl = require('../controllers_v2/review_controller');
-var labtestImportCtrl = require('../controllers_v2/labtestImport_controller');
->>>>>>> e6fe93318624b841b2b8d43610dac484be8b2832
-=======
 var reviewCtrl = require('../controllers_v2/review_controller')
 var labtestImportCtrl = require('../controllers_v2/labtestImport_controller')
->>>>>>> e3bdbdd0d2c3fdc7b38dd7c05044066c426100ef
 
 module.exports = function (app, webEntry, acl) {
   // app.get('/', function(req, res){
@@ -38,86 +29,10 @@ module.exports = function (app, webEntry, acl) {
   // });
 
   // csq
-<<<<<<< HEAD
-  app.post(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.addUserRoles(acl))
-  app.post(version + '/acl/removeUserRoles', tokenManager.verifyToken(), aclsettingCtrl.removeUserRoles(acl))
-  app.get(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.userRoles(acl))
-  app.get(version + '/acl/userRole', tokenManager.verifyToken(), aclsettingCtrl.hasRole(acl))
-
-  app.get(version + '/acl/roleUsers', tokenManager.verifyToken(), aclsettingCtrl.roleUsers(acl))
-  app.post(version + '/acl/roleParents', tokenManager.verifyToken(), aclsettingCtrl.addRoleParents(acl))
-  app.post(version + '/acl/removeRoleParents', tokenManager.verifyToken(), aclsettingCtrl.removeRoleParents(acl))
-  app.post(version + '/acl/removeRole', tokenManager.verifyToken(), aclsettingCtrl.removeRole(acl))
-
-  app.post(version + '/acl/allow', tokenManager.verifyToken(), aclsettingCtrl.allow(acl))
-  app.post(version + '/acl/removeAllow', tokenManager.verifyToken(), aclsettingCtrl.removeAllow(acl))
-  app.get(version + '/acl/allow', tokenManager.verifyToken(), aclsettingCtrl.allowedPermissions(acl))
-  app.get(version + '/acl/isAllowed', tokenManager.verifyToken(), aclsettingCtrl.isAllowed(acl))
-
-  app.post(version + '/acl/removeResource', tokenManager.verifyToken(), aclsettingCtrl.removeResource(acl))
-  app.get(version + '/acl/areAnyRolesAllowed', tokenManager.verifyToken(), aclsettingCtrl.areAnyRolesAllowed(acl))
-  app.get(version + '/acl/resources', tokenManager.verifyToken(), aclsettingCtrl.whatResources(acl))
-
-  // wf
-<<<<<<< HEAD
-  app.get(version + '/alluser/userList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(0))
-  app.get(version + '/alluser/doctorList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(1))
-  app.get(version + '/alluser/patientList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(2))
-  app.get(version + '/alluser/nurseList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(3))
-  app.get(version + '/alluser/insuranceList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(4))
-  app.get(version + '/alluser/healthList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(5))
-  app.get(version + '/alluser/adminList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(6))
-  app.post(version + '/alluser/alluser', tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.updateAlluserList)
-
-  app.post(version + '/alluser/register', tokenManager.verifyToken(), alluserCtrl.registerTest(acl), getNoMid.getNo(1), alluserCtrl.register(acl))
-  app.post(version + '/alluser/cancelUser', tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.cancelAlluser)
-  app.post(version + '/alluser/unionid', tokenManager.verifyToken(), alluserCtrl.setOpenId, alluserCtrl.checkBinding, alluserCtrl.setOpenIdRes)
-  app.post(version + '/alluser/openId', tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.setMessageOpenId)
-  app.get(version + '/alluser/openId', tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.getMessageOpenId)
-  app.post(version + '/alluser/reset', tokenManager.verifyToken(), alluserCtrl.reset)
-  app.post(version + '/alluser/login', tokenManager.verifyToken(), alluserCtrl.openIdLoginTest, alluserCtrl.checkBinding, alluserCtrl.login)
-  app.post(version + '/alluser/logout', tokenManager.verifyToken(), alluserCtrl.logout)
-  app.get(version + '/alluser/userID', tokenManager.verifyToken(), alluserCtrl.getAlluserID)
-  app.post(version + '/alluser/sms', tokenManager.verifyToken(), alluserCtrl.sendSMS)
-  app.get(version + '/alluser/sms', tokenManager.verifyToken(), alluserCtrl.verifySMS)
-  app.get(version + '/alluser/agreement', tokenManager.verifyToken(), alluserCtrl.getAlluserAgreement)
-  app.post(version + '/alluser/agreement', tokenManager.verifyToken(), alluserCtrl.updateAlluserAgreement)
-=======
-  app.get(version + '/alluser/userList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(0));
-  app.get(version + '/alluser/doctorList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(1));
-  app.get(version + '/alluser/patientList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(2));
-  app.get(version + '/alluser/nurseList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(3));
-  app.get(version + '/alluser/insuranceList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(4));
-  app.get(version + '/alluser/healthList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(5));
-  app.get(version + '/alluser/adminList',  tokenManager.verifyToken(), alluserCtrl.getAlluserList(6));
-  app.post(version + '/alluser/alluser',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.updateAlluserList);
-  
-  app.post(version + '/alluser/register', alluserCtrl.registerTest(acl),getNoMid.getNo(1), alluserCtrl.register(acl));
-  app.post(version + '/alluser/cancelUser',  tokenManager.verifyToken(), alluserCtrl.checkAlluser,alluserCtrl.cancelAlluser);
-  app.post(version + '/alluser/unionid',  tokenManager.verifyToken(), alluserCtrl.setOpenId, alluserCtrl.checkBinding, alluserCtrl.setOpenIdRes);
-  app.post(version + '/alluser/openId',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.setMessageOpenId);
-  app.get(version + '/alluser/openId',  tokenManager.verifyToken(), alluserCtrl.checkAlluser, alluserCtrl.getMessageOpenId);
-  app.post(version + '/alluser/reset',  tokenManager.verifyToken(), alluserCtrl.reset);
-  app.post(version + '/alluser/login',  tokenManager.verifyToken(), alluserCtrl.openIdLoginTest,alluserCtrl.checkBinding,alluserCtrl.login);
-  app.post(version + '/alluser/logout',   tokenManager.verifyToken(), alluserCtrl.logout);
-  app.get(version + '/alluser/userID',   tokenManager.verifyToken(), alluserCtrl.getAlluserID);
-  app.post(version + '/alluser/sms',   tokenManager.verifyToken(), alluserCtrl.sendSMS);
-  app.get(version + '/alluser/sms',   tokenManager.verifyToken(), alluserCtrl.verifySMS);
-  app.get(version + '/alluser/agreement',   tokenManager.verifyToken(), alluserCtrl.getAlluserAgreement);
-  app.post(version + '/alluser/agreement',   tokenManager.verifyToken(), alluserCtrl.updateAlluserAgreement);
->>>>>>> e6fe93318624b841b2b8d43610dac484be8b2832
-
-  // gy
-  // review
-  app.post(version + '/review/reviewInfo', tokenManager.verifyToken(), reviewCtrl.postReviewInfo)
-  app.get(version + '/review/certificate', tokenManager.verifyToken(), reviewCtrl.getCertificate)
-  app.get(version + '/review/reviewInfo', tokenManager.verifyToken(), reviewCtrl.getReviewInfo)
-=======
   app.post(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.addUserRoles(acl), alluserCtrl.changerole)
   app.post(version + '/acl/removeUserRoles', tokenManager.verifyToken(), aclsettingCtrl.removeUserRoles(acl), alluserCtrl.changerole)
   app.get(version + '/acl/userRoles', tokenManager.verifyToken(), aclsettingCtrl.userRoles(acl))
   app.get(version + '/acl/userRole', tokenManager.verifyToken(), aclsettingCtrl.hasRole(acl))
->>>>>>> e3bdbdd0d2c3fdc7b38dd7c05044066c426100ef
 
   app.get(version + '/acl/roleUsers', tokenManager.verifyToken(), aclsettingCtrl.roleUsers(acl))
   app.post(version + '/acl/roleParents', tokenManager.verifyToken(), aclsettingCtrl.addRoleParents(acl))
@@ -129,18 +44,9 @@ module.exports = function (app, webEntry, acl) {
   app.get(version + '/acl/allow', tokenManager.verifyToken(), aclsettingCtrl.allowedPermissions(acl))
   app.get(version + '/acl/isAllowed', tokenManager.verifyToken(), aclsettingCtrl.isAllowed(acl))
 
-<<<<<<< HEAD
-  // niaodaifu
-<<<<<<< HEAD
-  app.get('/devicedata/niaodaifu/loginparam', niaodaifuCtrl.getLoginParam)
-}
-=======
-  app.get('/devicedata/niaodaifu/loginparam', niaodaifuCtrl.getLoginParam);
-=======
   app.post(version + '/acl/removeResource', tokenManager.verifyToken(), aclsettingCtrl.removeResource(acl))
   app.get(version + '/acl/areAnyRolesAllowed', tokenManager.verifyToken(), aclsettingCtrl.areAnyRolesAllowed(acl))
   app.get(version + '/acl/resources', tokenManager.verifyToken(), aclsettingCtrl.whatResources(acl))
->>>>>>> e3bdbdd0d2c3fdc7b38dd7c05044066c426100ef
 
   // wf
   app.get(version + '/alluser/userList', tokenManager.verifyToken(), alluserCtrl.getAlluserList(0))
@@ -181,10 +87,6 @@ module.exports = function (app, webEntry, acl) {
   app.get(version + '/labtestImport/photoByLabtest', tokenManager.verifyToken(), labtestImportCtrl.photoByLabtest)
   app.post(version + '/labtestImport/labelphoto', labtestImportCtrl.pullurl, labtestImportCtrl.pushurl, labtestImportCtrl.checkImportStatus, labtestImportCtrl.updateUserLatest)
 
-<<<<<<< HEAD
->>>>>>> e6fe93318624b841b2b8d43610dac484be8b2832
-=======
   // niaodaifu
   app.get('/devicedata/niaodaifu/loginparam', niaodaifuCtrl.getLoginParam)
 }
->>>>>>> e3bdbdd0d2c3fdc7b38dd7c05044066c426100ef
