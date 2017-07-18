279628899f1df96c732cdcd44b9b5fdb03a7d725
SageChi <sqchi1991@163.com>
2017-07-18 15:56:01 +0800

    cahce

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -35,13 +35,13 @@ exports.insertOrder = function(req, res, next) {
         else{
             var true_money;
             if(req.body.class == '01'){
-                true_money = doctor.charge1;
+                true_money = doctor.charge1 * 100;
             }
             else if(req.body.class == '02'){
-                true_money = doctor.charge2;
+                true_money = doctor.charge2 * 100;
             }
             else if(req.body.class == '03'){
-                true_money = doctor.charge2 - doctor.charge1;
+                true_money = doctor.charge2 * 100 - doctor.charge1 * 100;
             }
             else{
                 return res.status(403).send('服务类型不存在!');
@@ -114,6 +114,7 @@ exports.checkPayStatus = function (type) {
             if (err) {
                 return res.status(500).send(err);
             }
+            console.log(item)
             if (item === null) {
                 return res.status(400).send('不存在的订单');
             }
