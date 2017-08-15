10dbf36010a7ae449383c469c74533840b095ec3
xiaopangjiang <zjujiangyifan@163.com>
2017-08-15 22:26:22 +0800

    bug修改

--- middlewares/getNoMid.js
+++ middlewares/getNoMid.js
@@ -64,16 +64,19 @@ exports.getNo = function (setType) {
             _KeyDate = _targetDate
           }
           var query1 = {type: _numberingType, date: _KeyDate}
+          console.log(query1)
           Numbering.updateOne(query1, {$inc: {number:1}}, function (err, item1) {
             if (err) {
               return res.status(500).send(err.errmsg)
             }
             _TrnNumberingData = item1
+            console.log(_TrnNumberingData)
             if (_TrnNumberingData == null) {
               _TrnNumberingNo = 0
             } else {
               _TrnNumberingNo = _TrnNumberingData.number
             }
+            console.log(_TrnNumberingNo)
             _TrnNumberingNo = _TrnNumberingNo + 1
             var _Seq = _TrnNumberingNo
             if (_AlphamericFlag === 1) {
@@ -95,7 +98,7 @@ exports.getNo = function (setType) {
             req.newId = _Ret
             return next()
 
-          })
+          }, {upsert: true})
         }
       })
     }
