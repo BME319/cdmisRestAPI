1f8dbab54de7cc587925c9c9ba588c7471ba49d6
gy-jerry <920680270@qq.com>
2017-08-17 10:36:47 +0800

    insurance debug

--- controllers/insurance_controller.js
+++ controllers/insurance_controller.js
@@ -96,25 +96,24 @@ exports.updateMsgCount = function(req, res, next) {
 
 	InsuranceMsg.getOne(query, function(err, item) {
 		if (err) {
-      		return res.status(500).send(err.errmsg);
-    	}
-
-    	var upObj = {count: item.insuranceMsg.length};
-    	InsuranceMsg.updateOne(query, upObj, function(err, upInsMsg) {
-			if (err){
-				return res.status(422).send(err.message);
-			}
-			else if (upInsMsg == null) {
-				return res.json({result:'修改失败'})
-			}
-			else {
-				// return res.json({result: '修改成功', results:upInsMsg});
-				req.body.InsMsg = upInsMsg;
-				next();
-			}
-		}, {new: true});
-
-    	// res.json({results:item});
+      return res.status(500).send(err.errmsg);
+    } else if (item.insuranceMsg.constructor === Array) {
+			var upObj = {count: item.insuranceMsg.length};
+			InsuranceMsg.updateOne(query, upObj, function(err, upInsMsg) {
+				if (err){
+					return res.status(422).send(err.message);
+				} else if (upInsMsg == null) {
+					return res.json({result:'修改失败'})
+				} else {
+					// return res.json({result: '修改成功', results:upInsMsg});
+					req.body.InsMsg = upInsMsg;
+					next();
+				}
+			}, {new: true});
+			// res.json({results:item});
+		} else {
+			return res.status(400).send('no_insuranceMsg_available')
+		}
 	}, opts, fields, populate);
 }
 
