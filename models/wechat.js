34c71cd68aa78c1015e5b571e45b322b90f00de3
SageChi <sqchi1991@163.com>
2017-08-23 18:48:35 +0800

    cache

--- models/wechat.js
+++ models/wechat.js
@@ -59,8 +59,8 @@ Wechat.baseTokenManager = function (type) {
   // console.log(type || 'access_token');
   return function (req, res, next) {
     // console.log(req.headers);
-    // var query = {type: type || 'access_token', role: req.query.role || req.body.role}
-    var query = {type: type || 'access_token', role: req.session.role}
+    var query = {type: type || 'access_token', role: req.query.role || req.body.role}
+    // var query = {type: type || 'access_token', role: req.session.role}
     var appid = req.wxApiUserObject.appid
     var secret = req.wxApiUserObject.appsecret
 
@@ -103,8 +103,8 @@ Wechat.baseTokenManager = function (type) {
               jsapi_ticket: body2.ticket,
               api_ticket: 'body3.ticket',
               type: type || 'access_token',
-              // role: req.query.role || req.body.role
-              role: req.session.role
+              role: req.query.role || req.body.role
+              // role: req.session.role
             }, function (err, tokenObject) {
               if (err) return res.status(401).send('微信令牌保存失败!')
               req.wxToken = tokenObject
