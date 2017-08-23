0858dadb85750bde90e58927f9f3e280ba6961ed
wf19920502 <zjuwf2010@163.com>
2017-08-23 15:54:49 +0800

    Signed-off-by: wf19920502 <zjuwf2010@163.com>

--- controllers_v2/news_controller.js
+++ controllers_v2/news_controller.js
@@ -51,8 +51,8 @@ exports.getNewsByReadOrNot = function (req, res) {
   var userRole = req.session.role
   // console.log(userRole)
   // var userRole = req.session.role
-  var type = req.query.type
-  var _readOrNot = req.query.readOrNot
+  var type = Number(req.query.type)
+  var _readOrNot = Number(req.query.readOrNot)
 
   var query = {}
 
@@ -65,12 +65,12 @@ exports.getNewsByReadOrNot = function (req, res) {
   query['userId'] = userId
   // query['readOrNot'] = _readOrNot
   // if (userRole !== '' && userRole !== undefined) {
-  query['userRole'] = userRole
+  // query['userRole'] = userRole
   // }
   query['readOrNot'] = _readOrNot
     // 注意'_id'的生成算法包含时间，因此直接用'_id'进行降序排列
   var opts = {'sort': '-time'}
-
+  // console.log(query)
   News.getSome(query, function (err, items) {
     if (err) {
       return res.status(500).send(err.errmsg)
