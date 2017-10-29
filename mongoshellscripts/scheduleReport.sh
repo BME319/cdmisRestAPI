750621d66fa63f07745a3087c6f08ceaa67232c9
BME319 <zjubme319@163.com>
2017-10-29 16:04:35 +0800

    Update scheduleReport.sh

--- mongoshellscripts/scheduleReport.sh
+++ mongoshellscripts/scheduleReport.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 
-mongo --port 28000 /mnt/docker_test/cdmisRestAPI/mongoshellscripts/insertReport.js >> /mnt/docker_test/cdmisRestAPI/mongoscriptslogs/CDK_Report$(date +%Y%m%d).log 2>&1 &
+mongo --port 28000 /mnt/codes/cdmisRestAPI/mongoshellscripts/insertReport.js >> /mnt/codes/cdmisRestAPI/mongoscriptslogs/CDK_Report$(date +%Y%m%d).log 2>&1 &
